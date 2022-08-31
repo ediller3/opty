5c33380d44b02baf35311a8e222d105a57306a7f
Edward Diller <eddiller@mac.com>
2022-08-31 13:33:01 -0700

    Added pandas tool to flatten options chain

--- opty.py
+++ opty.py
@@ -1,7 +1,10 @@
 from tda import auth, client
+import tda
 import json
 import config
 import pandas as pd
+import httpx
+import datetime
 
 # authenticate
 try:
@@ -12,4 +15,21 @@ except FileNotFoundError:
         c = auth.client_from_login_flow(
             driver, config.api_key, config.redirect_uri, config.token_path)
  
-c.get_option_chain('AAPL')
+c.set_enforce_enums(False)
+
+opt_chain = []
+options = c.get_option_chain('AAPL',contract_type="CALL",strike_count=10,from_date=datetime.date(2022,9,1),to_date=datetime.date(2022,9,3))
+assert options.status_code == httpx.codes.OK, options.raise_for_status()
+query = options.json()
+
+for contr_type in ['callExpDateMap', 'putExpDateMap']:
+    contract = dict(query)[contr_type]
+    expirations = contract.keys()
+    for expiry in list(expirations):
+        strikes = contract[expiry].keys()
+        for st in list(strikes):
+            entry = contract[expiry][st][0]
+            opt_chain.append(entry)
+df = pd.DataFrame(opt_chain)
+print(df.columns)
+print(df[['putCall','symbol','daysToExpiration','strikePrice','bid','ask','last','volatility']])
