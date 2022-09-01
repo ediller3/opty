340146702796f1d43255b523506fca252d15625b
Edward Diller <eddiller@mac.com>
2022-08-31 18:07:11 -0700

    Added CLI for navigating options chain.

--- opty.py
+++ opty.py
@@ -5,20 +5,22 @@ import config
 import pandas as pd
 import httpx
 import datetime
+import pprint
 
 
-# authenticate
+# TDA Ameritrade Token Authentication
 try:
+    #Authenticate using token if token exists
     c = auth.client_from_token_file(config.token_path, config.api_key)
 except FileNotFoundError:
+    #Use Chromium webdriver to authenticate with TDA login
     from selenium import webdriver
     with webdriver.Chrome(executable_path="/Users/edwarddiller/Python/Opty/chromedriver") as driver:
         c = auth.client_from_login_flow(
             driver, config.api_key, config.redirect_uri, config.token_path)
-    print('ERROR: must authenticate')
-    ASSERT(False)
+    print('ERROR: Authentication failed.')
     
-# get options chain, return json object
+# Get options chain, return JSON object
 def get_opts(ticker, c_type='ALL', s_count=None, f_date=None, t_date=None):
     c.set_enforce_enums(False)
     #options = c.get_option_chain('AAPL',contract_type="CALL",strike_count=10,from_date=datetime.date(2022,9,1),to_date=datetime.date(2022,9,3))
@@ -27,7 +29,7 @@ def get_opts(ticker, c_type='ALL', s_count=None, f_date=None, t_date=None):
     query = options.json()
     return query
 
-# parse options chain, return panda dataFrame
+# Parse options chain, return pandas dataFrame
 def parse(query):
     opt_chain = []
     for contr_type in ['callExpDateMap', 'putExpDateMap']:
@@ -39,19 +41,38 @@ def parse(query):
                 entry = contract[expiry][st][0]
                 opt_chain.append(entry)
     df = pd.DataFrame(opt_chain)
-    return df
-def getBasicInput():
-    ticker = input("enter the ticker: ")
-    putOrCall = input("Specify PUT or CALL or nothing for both: ")
+    return df, contract
+
+# Get ticker and Put/Call input from user
+def getInput():
+    ticker = input("Enter a valid ticker: ")
+    putOrCall = input("Specify 'PUT', 'CALL' or leave blank for both: ")
     while putOrCall not in ['', 'PUT', 'CALL']:
         putOrCall = input("Please select ONLY PUT or CALL or nothing: ")
     if putOrCall == '': putOrCall = 'ALL'
     return [ticker, putOrCall]
 
-    
-args = getBasicInput()
+# Get expiration date input from user
+def getInputExpiry(df, contract):
+    exp_frame = pd.DataFrame(contract.keys(),columns=["Expiration Date"])
+    print(exp_frame)
+    rows = exp_frame.size
+    exp_i = int(input("Select an expiration date from list above (left column digit): "))
+    while exp_i < 0 or exp_i > (rows - 1):
+            exp_i = input("Select an expiration date from list above (left column digit): ")
+
+    #Find chosen date:
+    date_str = list(contract.keys())[exp_i] #Select date from contract keys in str format
+    strike = list(contract[date_str].values())[0] #Choose dummy strike from list of strikes for selected date
+    date_unix = strike[0]['expirationDate'] #Open list and return expirationDate in Unix format 
+
+    #Print all strikes for chosen date:
+    pd.set_option('display.max_rows', None)
+    date_df = df.loc[df['expirationDate'] == date_unix]
+    date_df.reset_index(inplace=True) #Resets row indexes
+    print(date_df[['strikePrice','putCall','symbol','expirationDate','bid','ask','last','volatility']])
+
+args = getInput()
 q = get_opts(args[0], c_type=args[1])
-df = parse(q)
-
-
-print(df[['putCall','symbol','daysToExpiration','strikePrice','bid','ask','last','volatility']])
+df, contract = parse(q)
+getInputExpiry(df, contract)
